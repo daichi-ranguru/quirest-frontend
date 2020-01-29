d12500d20d68b2e7050a23dfcd5839287a778891
daichi-ranguru <langr.ondrej.work@gmail.com>
2020-01-29 12:01:54 +0100

    Added dynamic nav in header

--- src/components/header.js
+++ src/components/header.js
@@ -1,59 +1,32 @@
-import React from "react"
 import { Link } from "gatsby"
-const Header = ({ siteTitle, menuLinks }) => (
+import PropTypes from "prop-types"
+import React from "react"
+
+const Header = ({ siteTitle }) => (
   <header
     style={{
-      background: "rebeccapurple",
-      marginBottom: "1.45rem",
+      background: `rebeccapurple`,
+      marginBottom: `1.45rem`,
     }}
   >
     <div
       style={{
-        background: "rebeccapurple",
-        marginBottom: "1.45rem",
+        margin: `0 auto`,
+        maxWidth: 960,
+        padding: `1.45rem 1.0875rem`,
       }}
     >
-      <div
-        style={{
-          margin: "0 auto",
-          maxWidth: 960,
-          padding: "1.45rem 1.0875rem",
-          display: "flex",
-          justifyItems: "space-between",
-          alignItems: "center",
-        }}
-      >
-        <h1 style={{ margin: 0, flex: 1 }}>
-          <Link
-            to="/"
-            style={{
-              color: "white",
-              textDecoration: "none",
-            }}
-          >
-            {siteTitle}
-          </Link>
-        </h1>
-        <div>
-          <nav>
-            <ul style={{ display: "flex", flex: 1 }}>
-              {menuLinks.map(link => (
-                <li
-                  key={link.name}
-                  style={{
-                    listStyleType: `none`,
-                    padding: `1rem`,
-                  }}
-                >
-                  <Link style={{ color: `white` }} to={link.link}>
-                    {link.name}
-                  </Link>
-                </li>
-              ))}
-            </ul>
-          </nav>
-        </div>
-      </div>
+      <h1 style={{ margin: 0 }}>
+        <Link
+          to="/"
+          style={{
+            color: `white`,
+            textDecoration: `none`,
+          }}
+        >
+          {siteTitle}
+        </Link>
+      </h1>
     </div>
   </header>
 )
--- src/pages/index.js
+++ src/pages/index.js
@@ -7,9 +7,6 @@ import "../styles/global.css"
 
 const IndexPage = ({ data }) => (
   <Layout>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
     <ul>
       {data.allStrapiArticle.edges.map(document => (
         <li key={document.node.id}>
@@ -27,7 +24,6 @@ const IndexPage = ({ data }) => (
         </li>
       ))}
     </ul>
-    <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
 
